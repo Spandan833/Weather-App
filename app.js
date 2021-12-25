8f2adc3e388c9f91504fd8e685d93c1db671d2cb
SosukeAizen833 <Spandan833@gmai.com>
2021-12-26 00:13:40 +0530

    layout changes

--- app.js
+++ app.js
@@ -5,12 +5,24 @@ result.addEventListener('click',()=>{
     getWeather(city);
     
 })
-const content = document.getElementById('content')
-async function getWeather(city) {
 
-    await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a052a3120e18eac3ee88a81e1fee3918`).then(res =>{
+const content = document.getElementById('content')
+function getWeather(city) {
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a052a3120e18eac3ee88a81e1fee3918`).then(res =>{
         return res.json()
     }).then(res =>{
         console.log(res);
+        editCard(city,res);
     })
 }
+
+let editCard = (city,data)=>{
+    document.querySelector('.city').textContent = city;
+    document.querySelector('.temp').textContent = `${Math.floor(data['main']['temp']-273)}C`;
+    const ico = document.querySelector('.weather-icon');
+    ico.src = `http://openweathermap.org/img/wn/${data["weather"][0]["icon"]}@2x.png`;
+}
+window.addEventListener('DOMContentLoaded',()=>{
+    getWeather("Agartala")
+})
